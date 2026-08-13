5d2233093b315f6253c900c0aa8b5028eedfb8b6
pjmoo <00pjmoooo@gmail.com>
2026-08-13 16:11:16 +0900

    Initial commit

---
+++ src/test/java/org/example/secu/SecuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.secu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
