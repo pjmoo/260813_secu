f62f3aa1982853c7fac69b2d21020faa06a95e54
pjmoo <00pjmoooo@gmail.com>
2026-08-13 16:11:16 +0900

    feat: 시큐리티 설정(SecurityConfig), 로그인/회원가입 페이지 및 컨트롤러 매핑 추가

---
+++ src/main/java/org/example/secu/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package org.example.secu.config;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Map;
+
+@Configuration
+public class SecurityConfig {
+    // 1. password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder(
+            @Validated @NotBlank @Value("${app.security.encoding-id}") String encodingId) {
+        // bcrypt
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        Map<String, PasswordEncoder> enc = Map.of(
+                "bcrypt", new BCryptPasswordEncoder(),
+                "scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8(),
+                "argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+        return new DelegatingPasswordEncoder(encodingId, enc);
+    }
+
+    // 2. filter chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .authorizeHttpRequests(
+                        auth -> auth
+//                                .requestMatchers("/").permitAll()
+//                                .requestMatchers("/signup").permitAll()
+                                .requestMatchers("/", "/signup").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll())
+                .build();
+    }
+}
--- src/main/java/org/example/secu/controller/UserAccountController.java
+++ src/main/java/org/example/secu/controller/UserAccountController.java
@@ -3,6 +3,7 @@ package org.example.secu.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.secu.service.UserAccountService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class UserAccountController {
     private final UserAccountService userAccountService;
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "user/signup";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login";
+    }
 }
